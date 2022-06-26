bb7f87da56aae5d256e574cb1732eb81889e5828
Mika <amethyst602@gmail.com>
2022-06-26 22:32:05 +0800

    新增彈跳視窗組件

---
+++ src/components/Panel.js
@@ -0,0 +1,52 @@
+import {React,useState} from 'react'
+import {render} from 'react-dom'
+
+/* 
+    1. 一次渲染，隨即調用。
+    2. 裝載組件
+ */
+const _class ={
+    true: 'panel-wrapper active',
+    false:'panel-wrapper'
+}
+
+
+
+function Panel(props) {
+    const [xButton,setXbutton] = useState(false);
+
+    const close =( ) =>{
+        setXbutton(false)
+        }
+    const open = () =>{
+        setXbutton(true)
+    }
+
+  return (
+    <>
+      <div className={_class[xButton]}>
+        <div className="over-layer" 
+        onClick={close}
+        >
+            <div className="panel">
+                <div className="head">
+                    <span className="close" onClick={close}>
+                        x
+                    </span>
+                    <p className="has-text-centered">
+                        Children Component
+                    </p>
+                </div>
+            </div>
+        </div>
+      </div> 
+    </>
+  )
+}
+
+const _div = document.createElement('div')
+document.body.appendChild(_div)
+
+const _panel = render(<Panel/>, _div)
+console.log(_panel)
+export default _panel
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,30 @@
 import {React,useEffect,useState} from 'react';
 import axios from 'commons/axios';
+import {TransitionGroup,CSSTransition} from 'react-transition-group'
 import Product from 'components/Product'
 import Toolbox from 'components/Toolbox';
 import { jsonDataRun } from 'commons/helper';
-
+import Panel from 'components/Panel'
 
 
 
 function Products(props) {
-    const[getData,setGetData] = useState(jsonDataRun(jsonDataRun));
+    const[getData,setGetData] = useState(jsonDataRun(jsonDataRun)); // 存完整原始資料
+    const[getDataAll,setGetDataAll] = useState(jsonDataRun(jsonDataRun));  // 存變更的資料
     useEffect(() =>{
         axios.get('http://localhost:3003/products').then((response)=>{
-            setGetData(response.data)
+            setGetData(response.data);
+            setGetDataAll(response.data)
         })
     },[])
 
     const search = text =>{
         // console.log(text);
-        let searchResults = [...getData]
+        let searchResults = [...getDataAll]
         // console.log(searchResults)
         searchResults = searchResults.filter(p => {
              const matchArray = p.name.match(new RegExp(text, 'gi'))
-             console.log(matchArray)
+            //  console.log(matchArray)
 
              return(
                  !!matchArray
@@ -29,9 +32,11 @@ function Products(props) {
             
         })
         setGetData(searchResults)
-        console.log(getData);
+        // console.log(getData);
     }
-
+    const toAdd =()=>{
+        console.log(Panel)
+    }
   return (
     <>
     <div>
@@ -40,19 +45,27 @@ function Products(props) {
         />
         <div className='products'>
             <div className="columns is-multiline is-desktop">
-            {
-            getData.map( p =>{
-                return(
-                    <div className="column is-3" key={p.id}>
-                        <Product
-                            product={p}
-                        />
-                    </div>
+            <TransitionGroup component={null}>
+
+           
+                {
+                getData.map( p =>{
+                    return(
+                        <CSSTransition classNames={"product-fade"} timeout={300} key={p.id}>
+                            <div className="column is-3" key={p.id}>
+                                <Product
+                                    product={p}
+                                />
+                            </div>
+                        </CSSTransition>
+                        )
+                    }
                     )
                 }
-                )
-            }
+
+            </TransitionGroup>
             </div>
+            <button className="button is-primary add-btn" onClick={toAdd}>add</button>
         </div>
     </div>
 
